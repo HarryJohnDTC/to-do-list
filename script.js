9ae8104d32d396cce942f9e8409ea30d90383c7b
HarryJohnGeit <liva13octobre2010@gmail.com>
2024-11-25 16:19:18 +0300

    desactivation des boutons uodate et delete quand on modifie

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
+let taskContainer = document.getElementById("task");
 let addButton = document.getElementById("add");
 let taskInput = document.getElementById("taskInput");
-let taskContainer = document.getElementById("task");
-
 let taskToUpdate = null;
 
 // Ajouter une tâche
@@ -9,10 +8,12 @@ addButton.onclick = function () {
     let taskText = taskInput.value.trim();
 
     if (taskText !== "") {
-        if (taskToUpdate) { 
+        if (taskToUpdate) {
+            // Mise à jour de la tâche existante
+            enableDeleteUpdate();
             addButton.innerText = "Add";
             taskToUpdate.querySelector("span").textContent = taskText;
-            taskToUpdate = null; 
+            taskToUpdate = null;
         } else {
             // Ajout d'une nouvelle tâche
             let taskElement = document.createElement("div");
@@ -22,8 +23,8 @@ addButton.onclick = function () {
                 <input type="checkbox" class="task-checkbox">
                 <span>${taskText}</span>
                 <div>
-                <button class="update" onclick="updateTask(this)">Update</button>
-                <button class="delete" onclick="deleteTask(this)">X</button>
+                    <button class="update" onclick="updateTask(this)">Update</button>
+                    <button class="delete" onclick="deleteTask(this)">X</button>
                 </div>
             `;
 
@@ -47,11 +48,31 @@ function updateTask(button) {
     taskToUpdate = button.parentElement.parentElement;
     let taskText = taskToUpdate.querySelector("span").textContent;
     taskInput.value = taskText;
+    disableDeleteUpdate(); // Désactive les boutons "Update" et "Delete" sur toutes les tâches
 }
-//check
+
+// (cocher ou décocher)
 taskContainer.addEventListener("change", function (e) {
     if (e.target && e.target.classList.contains("task-checkbox")) {
         let taskElement = e.target.parentElement;
         taskElement.classList.toggle("checked", e.target.checked);
     }
 });
+
+// Désactiver les boutons "Update" et "Delete"
+function disableDeleteUpdate() {
+    let buttons = taskContainer.querySelectorAll(".update, .delete");
+    for (let button of buttons) {
+        button.disabled = true;
+        button.classList.add("disable");
+    }
+}
+
+// Activer les boutons "Update" et "Delete"
+function enableDeleteUpdate() {
+    let buttons = taskContainer.querySelectorAll(".update, .delete");
+    for (let button of buttons) {
+        button.disabled = false;
+        button.classList.remove("disable");
+    }
+}
